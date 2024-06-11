4033ba6bd6d1ed08694c719dd540392ba7bd9c0a
Anusha Dharmasena <cdan.dharmasena@gmail.com>
2024-06-11 14:37:38 +1000

    Test

--- app/components/VisitorId.tsx
+++ app/components/VisitorId.tsx
@@ -1,19 +1,19 @@
-import {useVisitorData} from '@fingerprintjs/fingerprintjs-pro-react'
+import { useVisitorData } from "@fingerprintjs/fingerprintjs-pro-react";
 
 export default function VisitorId() {
-  const {isLoading, error, data, getData} = useVisitorData(
-    {extendedResult: true},
-    {immediate: true}
-  )
+  const { isLoading, error, data, getData } = useVisitorData(
+    { extendedResult: true },
+    { immediate: true }
+  );
 
   return (
     <div>
-      <button onClick={() => getData({ignoreCache: true})}>
+      <button onClick={() => getData({ ignoreCache: true })}>
         Reload data
       </button>
-      <p>VisitorId: {isLoading ? 'Loading...' : data?.visitorId}</p>
+      <p>VisitorId: {isLoading ? "Loading..." : data?.visitorId}</p>
       <p>Full visitor data:</p>
       <pre>{error ? error.message : JSON.stringify(data, null, 2)}</pre>
     </div>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,18 @@
-import {
-  FpjsProvider,
-  // FingerprintJSPro
-} from '@fingerprintjs/fingerprintjs-pro-react'
-import VisitorId from "./components/VisitorId";
+import { FpjsProvider } from "@fingerprintjs/fingerprintjs-pro-react";
+// import VisitorId from "./components/VisitorId";
 
 export default function Home() {
   return (
     <FpjsProvider
       loadOptions={{
         apiKey: process.env.NEXT_PUBLIC_FINGERPRINT_API_KEY as string,
-        // endpoint: [
-        //   // "https://metrics.yourwebsite.com", 
-        //   FingerprintJSPro.defaultEndpoint
-        // ],
-        // scriptUrlPattern: [
-        //   // "https://metrics.yourwebsite.com/web/v<version>/<apiKey>/loader_v<loaderVersion>.js",
-        //   FingerprintJSPro.defaultScriptUrlPattern
-        // ],
-        region: "eu"
+        region: "eu",
       }}
     >
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          <VisitorId />
+          {/* <VisitorId /> */}
+          Hello
         </div>
       </main>
     </FpjsProvider>
