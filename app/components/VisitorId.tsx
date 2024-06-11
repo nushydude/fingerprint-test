1011a8d1357b704277761ead15ce50f568e557b5
Anusha Dharmasena <cdan.dharmasena@gmail.com>
2024-06-11 13:54:01 +1000

    add visitor id

---
+++ app/components/VisitorId.tsx
@@ -0,0 +1,19 @@
+import {useVisitorData} from '@fingerprintjs/fingerprintjs-pro-react'
+
+export default function VisitorId() {
+  const {isLoading, error, data, getData} = useVisitorData(
+    {extendedResult: true},
+    {immediate: true}
+  )
+
+  return (
+    <div>
+      <button onClick={() => getData({ignoreCache: true})}>
+        Reload data
+      </button>
+      <p>VisitorId: {isLoading ? 'Loading...' : data?.visitorId}</p>
+      <p>Full visitor data:</p>
+      <pre>{error ? error.message : JSON.stringify(data, null, 2)}</pre>
+    </div>
+  )
+}
